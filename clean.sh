89f20b0a3446abcc36fbebfc4578c5dbd63205d7
Blaine Lewis <blaine.lewis.1@outlook.com>
2015-03-11 13:05:19 -0700

    Cleaned up the directory

--- clean.sh
+++
@@ -1,5 +0,0 @@
-
-
-rm *.fifo log.txt
-killall launcher.py
-scp -r * 192.168.163.208:/home/blaine1/C496/.
---
+++ tester.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python2
+
+import time
+import subprocess
+
+RUNS = 10
+
+results = open("rsync_baseline.out", "w")
+
+command = "rsync -a blaine1@cold07:~/test/ ~/test/"
+args = shlex.split(command)
+
+cleanup = "rm -rf ~/test"
+cleanup_args = shlex.split(cleanup)
+
+for i in range(RUNS):
+
+	start = time.time()
+
+	subprocess.call(args)
+
+	end = time.time()
+
+	results.write(str(end-start) + "\n")
+
+	subprocess.call(cleanup_args)
+
+
